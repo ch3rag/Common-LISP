7e27348031a0ad63f61ab01238f0dc209283a5f1
Bharat Singh Rajput <34980375+iambharatsingh@users.noreply.github.com>
2018-05-11 18:16:03 +0530

    Update Example 1 - sayHello.lisp

--- Example 1 - sayHello.lisp
+++ Example 1 - sayHello.lisp
@@ -1,1 +1,11 @@
+;Program To Read Name From User And Print Hello
 
+(format t "What's Your Name ? : ")
+
+(defvar *name* (read))
+;* * are used as naming convection for global variables
+
+(defun sayHello (*name*)
+	(format t "Hello, ~a!~%" *name*))
+
+(sayHello *name*)
