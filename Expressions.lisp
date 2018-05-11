dbf2e62e839e251d6a4c054d565fe0ce43151ab7
Bharat Singh Rajput <34980375+iambharatsingh@users.noreply.github.com>
2018-05-11 15:50:07 +0530

    Create Expressions.lisp

---
+++ Expressions.lisp
@@ -0,0 +1,19 @@
+; Expressions
+
+(write (+ 1 2 3 (* 4 5)))
+;26
+
+(write (/ 9 5))
+;9/5
+
+(write (/ 18 12))
+;3/2
+
+(write (* 10 (/ 9 5)))
+;18
+
+;Single Quotes are used to Supress Evalution
+
+(write '(+ 1 2 3))
+;(+ 1 2 3)
+
